a3a93f3f152f6e4caf79ba10c9cd0c7d2bf7cc95
griefbucket <lcampbell@ironclad.mobi>
2011-04-17 12:33:31 +0000

    Also install the .a files

--- install-freebsd.sh
+++ install-freebsd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 install -v -g wheel -m 444 -o root include/* /usr/local/include
-install -v -g wheel -m 444 -o root *.so /usr/local/lib
+install -v -g wheel -m 444 -o root *.so *.a /usr/local/lib
